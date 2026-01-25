04818625aaf52c6367b575a32eecbff6229fdb50
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2026-01-25 19:14:20 +0100

    :pencil2: revert unwanted change

--- yescieval/__init__.py
+++ yescieval/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 __version__ = (Path(__file__).parent / "VERSION").read_text().strip()
 
-from .base import Rubric, Parser, Judge
+from .base import Rubric, Parser
 from .rubric import (Informativeness, Correctness, Completeness, Coherence, Relevancy,
                     Integration, Cohesion, Readability, Conciseness,
                     MechanisticUnderstanding, CausalReasoning, TemporalPrecision, GapIdentification, 
